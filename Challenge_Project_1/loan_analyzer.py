461cf106008fe8cfa7a8d82775ea17ca6fa3dc76
Abhishika Fatehpuria <66853259+Abhishikaf@users.noreply.github.com>
2021-09-23 10:52:30 -0600

    Completed part 3 of the challenge project
    added automation till if then conditional loop

--- Challenge_Project_1/loan_analyzer.py
+++ Challenge_Project_1/loan_analyzer.py
@@ -19,15 +19,26 @@ loan_costs = [500, 600, 200, 1000, 450]
 # Print the number of loans from the list
 # YOUR CODE HERE!
 
+total_number_loans = len(loan_costs)
+print(f"The total number of loans are: {total_number_loans}")
+
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
 
+total_loan_value = sum(loan_costs)
+print("The total value of loans is: $", total_loan_value)
+
+
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
+
+average_loan_price = total_loan_value / total_number_loans
+print("The average loan amount is: $", average_loan_price)
+
 
 """Part 2: Analyze Loan Data.
 
@@ -64,6 +75,10 @@ loan = {
 # Print each variable.
 # YOUR CODE HERE!
 
+future_value = loan.get("future_value")
+remaining_months = loan.get("remaining_months")
+
+print(f"The future value of the loan is: ${future_value} and the time remaining on the loan in months is: {remaining_months} months")
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
 # Use a minimum required return of 20% as the discount rate.
@@ -72,12 +87,29 @@ loan = {
 
 # YOUR CODE HERE!
 
+min_return_rate = 0.2 # defining min return rate of 20%
+
+disc_rate = min_return_rate  # assigning the discout rate as the minimun required return rate
+
+present_value = future_value/(1 + disc_rate/12)**remaining_months
+
+print(f"The present value of the loan is: ${present_value}")
+
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 # YOUR CODE HERE!
 
+loan_cost = loan.get("loan_price")
+
+if(present_value >= loan_cost):
+    print("The loan is worth at least the cost to buy it")
+
+elif(present_value < loan_cost):
+    print("The loan is too expensive and not worth the price")
+else:
+    print("Error")
 
 """Part 3: Perform Financial Calculations.
 
